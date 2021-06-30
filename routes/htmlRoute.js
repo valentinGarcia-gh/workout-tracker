8eb20b12c93253d84c46531d3b25c99cea2cb4a9
Valentin Garcia <vgarcia0587@icloud.com>
2021-06-29 22:11:39 -0400

    updated readme

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,11 +2,6 @@ var path = require("path");
 var express = require("express");
 var router = express.Router();
 
-// router.use(function timeLog(req, res, next) {
-//   console.log("Time: ", Date.now());
-//   next();
-// });
-
 router.get("/exercise", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
